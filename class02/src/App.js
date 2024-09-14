0e73dea8f4e03ad2caf17eebbb30e6147820cd06
Binyameen <mbinyameen49fsd@gmail.com>
2024-09-14 19:46:33 +0500

    reactclass03

---
+++ class02/src/App.js
@@ -0,0 +1,24 @@
+// import Header from "./components/header/Header";
+// import Footer from "./components/footer/Footer";
+// import Button from "./components/button/Button";
+// import { Input1, Input2 } from "./components/input/Input";
+// import './app.css';
+// import bgImage from "./assets/bg.jpg";
+
+// function App() {
+//   return (
+//     <div id="home">
+//       <Header />
+//       <Button title="Any Thing As Prop"/>
+//       {/* <img src={bgImage} style={{width: "50%"}} /> */}
+//       <img src={require("./assets/bg.jpg")} style={{width: "50%"}} />
+//       <img src="/logo512.png" style={{width: "20%"}} />
+//       <h1>Hello World</h1>
+//       <Footer />
+//       <Input1 />
+//       <Input2 />
+//     </div>
+//   )
+// }
+
+// export default App;
